cecf35e469648596c7347b90384acb7519e04e39
Sophia Ju <sophia.ju10@gmail.com>
2022-03-25 19:51:36 -0400

    directory setup and initial get_data script

---
+++ src/get_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+
+data = pd.read_csv('https://twtransfer.energytransfer.com/ipost/capacity/operationally-available?f=csv&extension=csv&asset=TW&gasDay=01%2F24%2F2022&cycle=1&searchType=NOM&searchString=&locType=ALL&locZone=ALL')
+
+print(data[48:50]['DC'])
+
+data.to_csv(os.path.join('data','test_data.csv'), sep='\t', index=False)
